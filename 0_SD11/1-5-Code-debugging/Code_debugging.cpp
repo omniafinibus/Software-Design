b4cca1b9693157d6b08d960e3c7b0fb7e2d28ec5
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-23 16:06:07 +0200

    fix: add cmake to and update sd11 assignment 1-5

---
+++ 0_SD11/1-5-Code-debugging/Code_debugging.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, z;
+	
+	cout << "Value of x:";
+	cin >> x;
+	cout << "Value of y:";
+	cin >> y;
+	cout << "Value of z:";
+	cin >> z;
+	cout << "x = " << x << ", y = " << y << ", z = " << z << "\n";
+	cout << "x + y = " << x + y << '\n';
+	cout << "Sum of " << x << " and " << z << " is " << x + z << '\n';
+	cout << "y / x = " << y / x << '\n';
+	cout << "2 times " << x << " = " << 2 * x << '\n';
+}
