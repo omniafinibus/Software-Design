2d78a2bb37f233906e1f113358c28c01b8695053
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-23 16:27:53 +0200

    fix: update and add cmake to sd11 assignment 1-7

---
+++ 0_SD11/1-7-Code-implementation/Code_implementation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1, num2, num3;
+	float average;
+	
+	num1 = 125;
+	num2 = 28;
+	num3 = -25;
+	average = (num1 + num2 + num3) / 3.0;
+
+	cout << "   num1 = " << num1 << "\n";
+	cout << "   num2 = " << num2 << "\n";
+	cout << "   num3 = " << num3 << "\n";
+	cout << "Average = " << average << "\n";
+}
