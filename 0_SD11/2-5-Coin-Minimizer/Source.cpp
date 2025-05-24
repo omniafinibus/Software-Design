07d3cbcb3f86f23103b7f803e4f128a20e23841b
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-24 19:00:01 +0200

    update code and add cmake for sd11 assignment 2-5

--- 0_SD11/2-5-Coin-Minimizer/Source.cpp
+++ 0_SD11/2-5-Coin-Minimizer/Source.cpp
@@ -2,62 +2,29 @@
 
 using namespace std;
 
+int getAmountOfCoins(int* total, int coinSize) {
+	int numOfCoins = (int) *total/coinSize;
+	*total -= numOfCoins*coinSize;
+	return numOfCoins;
+}
+
 int main()
 {
-	int twoEuro, oneEuro, fiftyCents, twentyCents, tenCents, fiveCents, twoCents, oneCents;
-	//if these variables are double, i doesn't work?
+	int amount, cents;
+	char separator;
+	int amountInCents;
 
-	double amountEuroInCent, amountEuro, amountCents;
-	int amountOneInCent, amountTwoInCent;
-	int hundredCentTotal, fiftyCentTotal, twentyCentTotal, tenCentTotal, fiveCentTotal, twoCentTotal, oneCentTotal;
-	int remainder;
+	//ask for amount of euros and cents separately
+	cout << "Amount of money: "; 
+	cin >> amount >> separator >> cents;
+	amountInCents = (int) amount*100 + cents;
 
-	//ask for amount of euros and eurocents seperatly
-	cout << "Amount of euros: ";
-	cin >> amountEuro;
-
-	cout << "Amount of eurocents: ";
-	cin >> amountCents;
-
-	//calculate amount of 2,1,0.50,0.20 etc coins there are
-	amountTwoInCent = amountCents / 200;
-	amountOneInCent = amountCents / 100;
-
-	twoEuro = amountEuro / 2 + amountTwoInCent;
-
-	oneEuro	= (amountOneInCent + amountEuro) - twoEuro * 2;
-	hundredCentTotal = amountOneInCent * 100;
-
-	remainder = amountCents - hundredCentTotal;
-
-	fiftyCents = remainder / 50;
-	remainder = remainder % 50;
-
-	twentyCents = remainder / 20;
-	remainder %= 20;
-	twentyCentTotal = twentyCents * 20;
-
-	tenCents = (amountCents - hundredCentTotal - fiftyCentTotal - twentyCentTotal) / 10;
-	tenCentTotal = tenCents * 10;
-
-	fiveCents = (amountCents - hundredCentTotal - fiftyCentTotal - twentyCentTotal - tenCentTotal) / 5;
-	fiveCentTotal = fiveCents * 5;
-
-	twoCents = (amountCents - hundredCentTotal - fiftyCentTotal - twentyCentTotal - tenCentTotal - fiveCentTotal) / 2;
-	twoCentTotal = twoCents * 2;
-
-	oneCents = (amountCents - hundredCentTotal - fiftyCentTotal - twentyCentTotal - tenCentTotal  - fiveCentTotal - twoCentTotal) / 1;
-
-	//display amount of minimum coins
-	cout << "2 euro coins:		" << (int) twoEuro << "\n";
-	cout << "1 euro coins:		" << (int) oneEuro << "\n";
-	cout << "50 eurocent coins:	" << (int) fiftyCents << "\n";
-	cout << "20 eurocent coins:	" << (int) twentyCents << "\n";
-	cout << "10 eurocent coins:	" << (int) tenCents << "\n";
-	cout << "5 eurocent coins:	" << (int) fiveCents << "\n";
-	cout << "2 eurocent coins:	" << (int) twoCents << "\n";
-	cout << "1 eurocent coins:	" << (int) oneCents << "\n";
-
-	system("pause");
+	int size[] = {200, 100, 50, 20, 10, 5, 2, 1};
 	
+	//display amount of minimum coins
+	for(int i; i < 8; i++){
+		cout << (float) size[i]/100 << " euro coins: ";
+		cout << getAmountOfCoins(&amountInCents, size[i]); 
+		cout << "\n";
+	}
 }
