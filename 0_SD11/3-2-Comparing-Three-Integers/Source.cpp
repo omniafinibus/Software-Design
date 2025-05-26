e548ca2e7741f6341af3a39a0936408f0d0ac47a
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-26 13:34:07 +0200

    fix: update code and add cmake for sd11 assignment 3-2

--- 0_SD11/3-2-Comparing-Three-Integers/Source.cpp
+++ 0_SD11/3-2-Comparing-Three-Integers/Source.cpp
@@ -10,19 +10,11 @@ int main()
 	cout << "Please enter 3 numbers: ";
 	cin >> int1 >> int2 >> int3;
 
-	//calculate the largest int or equel
 	//print out which is the largest or if they are equel
-	if (int1 == int2 == int3)
-	cout << "All numbers are equel.\n";
-
-	if (int1 > int2 && int1 > int3)
-	cout << int1 << " is the biggest number.\n";
-
-	if (int2 > int1 && int2 > int3)
-	cout << int2 << " is the biggest number.\n";
-
-	if (int3 > int1 && int3 > int2)
-	cout << int3 << " is the biggest number.\n";
-	
-	system("pause");
+	if (int1 == int2 && int1 ==  int3){
+		cout << "All numbers are equal.\n";
+	}
+	else {
+		cout << max(max(int1, int2), int3) << " is the biggest number.\n";
+	}
 }
