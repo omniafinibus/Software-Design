64b5e37a932af8f2282a8a7aaaf784c6094a2d05
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-26 16:16:56 +0200

    fix: update code and add cmake for sd11 assignment 4-5

--- 0_SD11/4-5-Wavelenghts/Source.cpp
+++ 0_SD11/4-5-Wavelenghts/Source.cpp
@@ -1,72 +1,45 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int wave;
-	int usable;
+	int minIndex, midIndex, maxIndex;
+	double wavelength;
+	double wavelengthThreshold[] = {0, 1e-11, 1e-8, 4e-7, 7e-7, 1e-3, 1e-1};;
+	string wavelengthNames[] = {"Gamma Rays", "X-Rays", "Ultraviolet", "Visible light", "Infrared", "Microwave", "Radio Waves"};
+	int numberOfWavelengths = sizeof(wavelengthThreshold)/sizeof(double);
 
-	//ask the user for a wavelength
 	cout << "Please enter a wavelength:\n";
-	cout << "If the wavelength is bigger than 1e-1, enter 1.\n";
-	cout << "If the wavelenght is smaller than 1e-1 and bigger than 1e-3, enter 2.\n";
-	cout << "If the wavelenght is smaller than 1e-3 and bigger than 7e-7, enter 3.\n";
-	cout << "If the wavelenght is smaller than 7e-1 and bigger than 4e-7, enter 4.\n";
-	cout << "If the wavelenght is smaller than 4e-7 and bigger than 1e-8, enter 5.\n";
-	cout << "If the wavelenght is smaller than 1e-8 and bigger than 1e-11, enter 6.\n";
-	cout << "If the wavelenght is smaller than 1e-11, enter 7.\n";
-	cin >> wave;
+	cin >> wavelength;
 
-	//workaround for maximum wavelength
-	/*if (wave >= 1e-1)
-	{
-		usable = 1;
-	}
-	else if (wave >= 1e-3)
-	{
-		usable = 2;
-	}
-	else if (wave >= 7e-7)
-	{
-		usable = 3;
-	}
-	else if (wave >= 4e-7)
-	{
-		usable = 4;
-	}
-	else if (wave >= 1e-8)
-	{
-		usable = 5;
-	}
-	else if (wave >= 1e-11)
-	{
-		usable = 6;
-	}
-	else if (wave < 1e-11)
-	{
-		usable = 7;
-	}*/
+	minIndex = 0;
+	midIndex = numberOfWavelengths / 2;
+	maxIndex = numberOfWavelengths-1;
 
-	//use a switch thingymajig to find the corresponding name and then print out this name
-	switch (wave)
-	{
-	case 1:
-		cout << "These are of the Radiowave variaty. \n";
-		break;
-	case 2:	cout << "These are of the Microwave variaty. \n";
-		break;
-	case 3:	cout << "These are of the Infrared variaty. \n";
-		break;
-	case 4:	cout << "These are of the Visible light variaty. \n";
-		break;
-	case 5:	cout << "These are of the Ultraviolet variaty. \n";
-		break;
-	case 6:	cout << "These are of the X-Ray variaty. \n";
-		break;
-	case 7:	cout << "These are of the Gamma ray variaty. \n";
-		break;
-	}
+	// Divide and conquer approach
+	for(int i = 0; i < numberOfWavelengths; i++) {
+		cout << "min: " << minIndex;
+		cout << " mid: " << midIndex;
+		cout << " max: " << maxIndex << "\n";
 
-	system("pause");
+		// See which limit needs to be updated
+		if(wavelength >= wavelengthThreshold[midIndex]){
+			minIndex = midIndex;
+		}
+		else{
+			maxIndex = midIndex-1;
+		}		
+		
+		if(maxIndex == minIndex){
+			// Value found
+			cout << "The wave is falls under the type " << wavelengthNames[minIndex] << "\n"; 
+			break;
+		}
+		else{
+			// Find the new midpoint (Rounding up)
+			midIndex = minIndex + (maxIndex-minIndex)/2 + 1;
+		}
+	}
 }
