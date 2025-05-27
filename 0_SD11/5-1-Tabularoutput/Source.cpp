128b923d14ad440b2d40a3a4b1a9e99c5d5763f4
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-27 14:11:57 +0200

    fix: update code and add cmake for sd11 assignment 5-1

--- 0_SD11/5-1-Tabularoutput/Source.cpp
+++ 0_SD11/5-1-Tabularoutput/Source.cpp
@@ -7,20 +7,16 @@ int main()
 {
 	int amount;
 
-	//ask for postive int value
-	do
-	{
-		cout << "Please enter a postive value:\n";
+	//ask for positive int value
+	do {
+		cout << "Please enter a positive value:\n";
 		cin >> amount;
 	} while (amount < 0);
 
 
 	//calculate and print out N N*N N*N*N N*N*N*N
 	cout << "N\tN*N\tN*N*N\tN*N*N*N\n";
-	for ( int N = 1; N <= amount; N++)
-	{
+	for ( int N = 1; N <= amount; N++) {
 		cout << pow(N, 1) << "\t" << pow(N, 2) << "\t" << pow(N, 3) << "\t" << pow(N, 4) << "\n";
 	}
-
-	system("pause");
 }
