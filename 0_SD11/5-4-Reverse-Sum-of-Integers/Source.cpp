0c1741206f50684f7e230aaa93eb70acb709f02c
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-27 14:32:34 +0200

    fix: update code and add cmake for sd11 assignment 5-4

--- 0_SD11/5-4-Reverse-Sum-of-Integers/Source.cpp
+++ 0_SD11/5-4-Reverse-Sum-of-Integers/Source.cpp
@@ -5,25 +5,27 @@ using namespace std;
 int main()
 {
 	int entered;
-	int n, a = 0;
+	int add = 0;
+	int total = 0;
 
-	//ask and check for an integer between 15 and 45
+	//ask and check for an integer above 15
 	do
 	{
-		cout << "Please enter an integer between 15 and 45:\n";
+		cout << "Please enter an integer greater than 15:\n";
 		cin >> entered;
-	} while (entered < 10 || entered > 45);
+	} while (entered < 15);
 
 	//use for loop to calculate value
-	for (n = 1; a <= entered; n++)
-	{
-		a += n;
-	}
+	
+	do {
+		total += ++add; 		
+	} while(total < entered);
 
-	//print out calculations and value
-	a -= n;
-	n -= 2;
-	cout << "1 + 2 + 3 + ... + " << n << " = " << entered << "\n";
+	cout << "1 + 2 + 3 + ... + " << add - 1 << " = " << total - add << "\n";
 
-	system("pause");
+	cout << total - add << " is less than " << entered << "\n";
+	
+	cout << "1 + 2 + 3 + ... + " << add << " = " << total<< "\n";
+
+	cout << total << " is greater or equal to " << entered << "\n";
 }
