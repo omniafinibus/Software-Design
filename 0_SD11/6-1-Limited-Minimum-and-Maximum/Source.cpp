401da615e0e8a69f5bc93ca5b550dc86a67094c1
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-27 14:42:39 +0200

    fix: update code and add cmake for sd11 assignment 6-1

--- 0_SD11/6-1-Limited-Minimum-and-Maximum/Source.cpp
+++ 0_SD11/6-1-Limited-Minimum-and-Maximum/Source.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	int ent, amount;
-	int entMax, entMin;
+	int entry, amount;
+	vector<int> vEntries;
 
-	//ask for amount of enteries
+	//ask for amount of entries
 	cout << "How many entries are there total?\n";
 	cin >> amount;
 
-	//ask for amount sold in a loop of ent.
-	//primer
-	cout << "Enter the amount of sold products for agent nr 1: ";
-	cin >> entMax;
-	entMin = entMax;
-
-	//loop
-	for (int i = 2; i <= amount; i++)
-	{
+	for (size_t i = 0; i < amount; i++) {
 		//ask for value
 		cout << "Enter the amount of sold products for agent nr " << i << ": ";
-		cin >> ent;
-
-		//check if the entered value is bigger or smaller then the previous one
-		if (ent > entMax)
-		{
-			entMax = ent;
-		}
-		else if (ent < entMax)
-		{
-			entMin = ent;
-		}
+		cin >> entry;
+		vEntries.push_back(entry);
 	}
 
 	//print out the winner and loser. 
-	cout << "The winner sold " << entMax << " products!\n";
-	cout << "The loser sold " << entMin << " products!\n";
-
-	system("pause");
+	cout << "The winner sold " << *max_element(vEntries.begin(), vEntries.end()) << " products!\n";
+	cout << "The loser sold " << *min_element(vEntries.begin(), vEntries.end()) << " products!\n";
 }
